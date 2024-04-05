e7dc9e55d961d930755e429bcb415961d4db7623
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-05 14:23:18 -0700

    Add purple grass and tree assets

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,44 +1,40 @@
 <script lang="ts">
   import { T } from "@threlte/core";
-  import {
-    Grid,
-    OrbitControls,
-    TransformControls,
-    useGltf,
-  } from "@threlte/extras";
+  import { OrbitControls, useGltf } from "@threlte/extras";
   import * as Three from "three";
-  import { DEG2RAD } from "three/src/math/MathUtils";
-
-  let y = 2;
 </script>
 
-<!-- Grid -->
-<!-- <Grid cellColor="#808080" sectionSize={0} /> -->
-
 <!-- Camera -->
-<T.PerspectiveCamera position={[20, 20, 20]} fov={50} makeDefault>
+<T.PerspectiveCamera position={[8, 8, 8]} fov={70} makeDefault>
   <!-- Controls -->
-  <OrbitControls enableDamping />
+  <OrbitControls enableRotate={false} minDistance={5} maxDistance={20} />
 </T.PerspectiveCamera>
 
 <!-- Lights the scene equally -->
-<T.AmbientLight color="#ffffff" intensity={3} />
+<T.AmbientLight color="#fefff5" intensity={1} />
 
 <!-- Light that casts a shadow -->
-<T.PointLight
+<T.DirectionalLight
   color="#fcffde"
-  intensity={0.2}
-  position={[10, 10, 0]}
-  shadow.camera.top={8}
+  intensity={2}
+  position={[20, 20, 10]}
   castShadow
 />
 
 <!-- Floor -->
-<T.Mesh rotation.x={DEG2RAD * 90} receiveShadow>
+<T.Mesh rotation.x={Math.PI / 2} receiveShadow>
   <T.PlaneGeometry args={[100000, 100000]} />
   <T.MeshStandardMaterial color="#95ed8e" side={Three.DoubleSide} />
 </T.Mesh>
 
 {#await useGltf("/assets/bunny.glb") then bunny}
-  <T is={bunny.scene} position={[0, 0.5, 0]} scale={0.4} />
+  <T is={bunny.scene} position={[0, 0.5, 0]} scale={0.5} />
+{/await}
+
+{#await useGltf("/assets/purple-grass.glb") then purpleGrass}
+  <T is={purpleGrass.scene} position={[-10, 0.5, 0]} scale={4} />
+{/await}
+
+{#await useGltf("/assets/tree.glb") then tree}
+  <T is={tree.scene} position={[10, 0.5, -4]} scale={2} />
 {/await}
